eff9cb8ebe5a12ca903bb4b15c4517f7872965da
fahrizaziz <188548284+ITelkomedika@users.noreply.github.com>
2026-02-26 09:43:27 +0700

    Membuat relasi baru di entity antara user dengan task update dan task dengan task update

--- src/task/entities/task-update.entity.ts
+++ src/task/entities/task-update.entity.ts
@@ -4,18 +4,32 @@ import {
   PrimaryGeneratedColumn,
   Column,
   CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
+import { Task } from './task.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @Entity('task_updates')
 export class TaskUpdate {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  task_id: number;
+  // =============================
+  // RELATION TO TASK
+  // =============================
+  @ManyToOne(() => Task, (task) => task.updates, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'task_id' })
+  task: Task;
 
-  @Column()
-  user_id: number;
+  // =============================
+  // RELATION TO USER
+  // =============================
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
 
   @Column('text')
   content: string;
